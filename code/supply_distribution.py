66de2fb045126ac0e0b33449dcb65c1f85b8838d
Lukas Kemmer <lukaskemmer@Lukass-MacBook-Pro-2.local>
2018-02-22 15:40:59 +0100

    changes parameters and small update of reset-method

--- code/supply_distribution.py
+++ code/supply_distribution.py
@@ -9,7 +9,7 @@ class SupplyDistribution:
 
     def __init__(self, n_stores=3, cap_truck=100, prod_cost=1, max_prod=10,
                  store_cost=np.array([0.01, 0.1, 0.1, 0.1]), truck_cost=np.array([2, 3, 4]),
-                 cap_store=np.array([20, 5, 5, 5]), penalty_cost=0.3, price=2, gamma=0.95):
+                 cap_store=np.array([20, 5, 5, 5]), penalty_cost=0.3, price=5, gamma=0.95):
         """
         :param n_stores:
         :param cap_truck:
@@ -49,7 +49,7 @@ class SupplyDistribution:
         self.s[0] = 5
         self.demand = np.zeros(self.n_stores, dtype=int)
         self.t = 0
-        return self.s
+        return self.s.copy()
 
     def step(self, action):  # TODO Check np.array * -- Droche 15/02
         self.s[0] = min(self.s[0] + action[0] - sum(action[1:]), self.cap_store[0])
