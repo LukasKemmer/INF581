237cdf4b7c30a8c20c3958c137a4d01a5df2412d
Lukas Kemmer <lukaskemmer@Lukass-MacBook-Pro-2.local>
2018-02-27 14:40:01 +0100

    debug this

--- code/supply_distribution.py
+++ code/supply_distribution.py
@@ -9,7 +9,7 @@ class SupplyDistribution:
 
     def __init__(self, n_stores=3, cap_truck=100, prod_cost=1, max_prod=10,
                  store_cost=np.array([0.01, 0.1, 0.1, 0.1]), truck_cost=np.array([2, 3, 4]),
-                 cap_store=np.array([20, 5, 5, 5]), penalty_cost=1, price=5, gamma=0.95):
+                 cap_store=np.array([20, 5, 5, 5]), penalty_cost=1, price=5, gamma=0.90):
         """
         :param n_stores:
         :param cap_truck:
@@ -60,10 +60,11 @@ class SupplyDistribution:
                   + np.sum(np.minimum(np.zeros(len(self.s)), self.s)) * self.penalty_cost # Changed to + so that penalty cost actually decrease reward -- Luke 26/02
                   - np.sum(np.ceil(action[1:] / self.cap_truck) * self.truck_cost)) # Removed .T after np.ceil, as it was unnecessary -- Luke 19/02
         info = "Demand was: ", self.demand  # TODO delete or do something -- Droche 15/02
+        state = np.hstack((self.s.copy(), self.demand.copy()))
         self.t += 1
         self.update_demand()
         done = 0
-        return np.hstack((self.s.copy(), self.demand.copy())), reward, done, info
+        return state, reward, done, info
 
     def update_demand(self):
         """
@@ -75,7 +76,7 @@ class SupplyDistribution:
         for i in range(self.n_stores):
             # We need an integer so we use the ceiling because if there is demand then we asume the users will buy
             # what they need and keep the rests. We use around to get an integer out of it.
-            demand[i] = int(np.ceil(1.5 * np.sin(2 * np.pi * self.t / 52 + i) + 1.5 + np.random.randint(0, 2)))
+            demand[i] = int(np.ceil(1.5 * np.sin(2 * np.pi * self.t / 24 + i) + 1.5 + np.random.randint(0, 2)))
         self.demand = demand
 
     def action_space_old(self): # Declared as "old" because in some states it would return an empty state space
@@ -99,9 +100,13 @@ class SupplyDistribution:
         :return: The posible actions in a list of tuples. Each tuple with (a0, a1, ..., ak) k = n_stores.
         """
         feasible_actions = []
-        for element in itertools.product(*[np.arange(0, np.min(s[0], self.cap_store[i])) for i in np.arange(0, self.n_stores+1)]):
+        a_0 = np.arange(0, self.max_prod + 1)
+
+        iterator = [a_0, *[np.arange(0, min(self.s[0], self.cap_store[i] - self.s[i] + 1)) for i in np.arange(1, self.n_stores+1)]]
+        for element in itertools.product(*iterator):
             if np.sum(element[1:]) <= self.s[0]:
                 feasible_actions.append(element)
+
         return np.array(feasible_actions)
 
     def observation_space(self):
