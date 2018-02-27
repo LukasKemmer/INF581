63a4118513a0e121b492dc1bee194999966c9666
Lukas Kemmer <lukaskemmer@Lukass-MacBook-Pro-2.local>
2018-02-27 13:58:15 +0100

    adds minor changes

--- code/evaluate_agent.py
+++ code/evaluate_agent.py
@@ -29,16 +29,22 @@ def print_step(step, state, action, reward, state_new, total_reward, freq = 100)
 np.random.seed(10107)
 
 # Simulation parameters
-n_episodes = 1
-max_steps = 104 # 2 years = 2 * 52 weeks
+n_episodes = 10000
+max_steps = 24 # 2 years = 2 * 52 weeks -- maybe better use 24 months = 2 years
 
 # Visualization parameters
-output=True
-status_freq = 1 # Print status (current episode) every X episodes
+output=0
+status_freq = 10 # Print status (current episode) every X episodes
 print_freq = 1 # Print current step every X episodes
 
 # Instantiate environment
-env = SupplyDistribution()
+store_cost = np.array([0.01, 0.1, 0.1, 0.1], dtype=np.float32)
+truck_cost = np.array([2, 3, 4], dtype=np.float32)
+cap_store=np.array([20, 5, 5, 5], dtype=np.int32)
+
+env = SupplyDistribution(n_stores=3, cap_truck=100, prod_cost=1, max_prod=10,
+                 store_cost=store_cost, truck_cost=truck_cost,
+                 cap_store=cap_store, penalty_cost=1, price=5, gamma=0.9)
 
 # Select agent
 #agent = q_s_agent(threshold = np.array([10, 3, 3, 3]), reorder_quantity = np.array([11, 3, 3, 3]))
@@ -55,19 +61,17 @@ for episode in np.arange(n_episodes):
     if episode % status_freq == 0:
         print("Episode ", episode)
 
-    # Reset environment and episode reward
+    # Reset environment, select initial action and reset episode reward
     state = env.reset()
-    episode_reward = 0
-    
-    # Select an action
     action = agent.get_action(state)
+    episode_reward = 0
     
     for step in np.arange(max_steps):
         
         # Update environment
         state_new, reward, done, info = env.step(action)
         
-        # Select an action
+        # Select a new action
         action_new = agent.get_action(state)
         
         # Update episode reward
@@ -90,6 +94,6 @@ for episode in np.arange(n_episodes):
 # ============================ 3. Output results ============================ #    
 
 # Output results
-print("Average reward: ", np.mean(rewards))
+print("Average reward: ", round(np.mean(rewards),2))
 plt.plot(rewards)
 plt.hist(rewards, normed=True)
