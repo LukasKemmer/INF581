21b2f8b60932c696ebcd34c9cdf3d85d0979f961
Lukas Kemmer <lukaskemmer@Lukass-MacBook-Pro-2.local>
2018-03-02 18:16:41 +0100

    adds new plot feature

--- code/evaluate_agent.py
+++ code/evaluate_agent.py
@@ -10,8 +10,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from q_s_agent import q_s_agent
 from approximate_sarsa import approximate_sarsa_agent
-from approximate_sarsa_V2 import approximate_sarsa_agent_V2
-from approximate_sarsa_V3 import approximate_sarsa_agent_V3
+#from approximate_sarsa_V2 import approximate_sarsa_agent_V2
+#from approximate_sarsa_V3 import approximate_sarsa_agent_V3
 from supply_distribution import SupplyDistribution
 from reinforce import REINFORCE_agent
 
@@ -31,12 +31,12 @@ def print_step(step, state, action, reward, state_new, total_reward, freq = 100)
 np.random.seed(10108)
 
 # Simulation parameters
-n_episodes = 5001
+n_episodes = 1000
 max_steps = 24  # 2 years = 52 * 2 weeks ( 2 week steps )
 
 # Visualization parameters
-output=1
-status_freq = 10 # Print status (current episode) every X episodes
+output=0
+status_freq = 100 # Print status (current episode) every X episodes
 print_freq = 100 # Print current step every X episodes
 
 # Instantiate environment
@@ -49,9 +49,9 @@ env = SupplyDistribution(n_stores=1, cap_truck=5, prod_cost=0, max_prod=10,
 # Select agent
 
 #agent = q_s_agent(threshold = np.array([10, 3, 3, 3]), reorder_quantity = np.array([11, 3, 3, 3]))
-#agent = approximate_sarsa_agent(env)
+agent = approximate_sarsa_agent(env)
 #agent = approximate_sarsa_agent_V2(env)
-agent = approximate_sarsa_agent_V3(env)
+#agent = approximate_sarsa_agent_V3(env)
 #agent = REINFORCE_agent(env,10,3, max_steps)
 
 # ============================ 2. Evaluate agent ============================ #
@@ -113,25 +113,8 @@ for episode in np.arange(n_episodes):
 # Output results
 print("Average reward: ", round(np.mean(rewards),2))
 
-# Receive information from agent
-ns = [agent.log[i][0] for i in range(len(agent.log))]
-alphas = [agent.log[i][1] for i in range(len(agent.log))]
-epsilons = [agent.log[i][2] for i in range(len(agent.log))]
-deltas = [agent.log[i][3] for i in range(len(agent.log))]
-thetas = [agent.log[i][4] for i in range(len(agent.log))]
-
-# Plot results
-fig = plt.figure(figsize=(5, 10), dpi=120)
-fig.add_subplot(6, 1, 1)
-plt.plot(rewards)
-fig.add_subplot(6, 1, 2)
-plt.plot(thetas)
-fig.add_subplot(6, 1, 3)
-plt.plot(deltas)
-fig.add_subplot(6, 1, 4)
-plt.plot(alphas)
-fig.add_subplot(6, 1, 5)
-plt.plot(epsilons)
+# Create plots from agent (e.g. parameter development over time)
+agent.plot(agent.log)
 
 # Plot some behavior
 seq = np.arange(24*900,24*902)
