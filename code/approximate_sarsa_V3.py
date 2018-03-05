81ff49f7d08449c3da47241839e06125eadc9487
Droche <ddegrimaudetderocheb@itba.edu.ar>
2018-03-05 14:23:04 +0100

    apporximate sarsa 3 more features

--- code/approximate_sarsa_V3.py
+++ code/approximate_sarsa_V3.py
@@ -7,6 +7,7 @@ Created on Sat Feb 24 13:06:37 2018
 #from supply_distribution import SupplyDistribution
 import numpy as np
 import numba as nb
+import matplotlib.pyplot as plt
 
 class approximate_sarsa_agent_V3(object):
     
@@ -15,12 +16,12 @@ class approximate_sarsa_agent_V3(object):
         self.env = env
         
         # Initialize theta random
-        self.theta = np.random.rand(29)
+        self.theta = np.random.rand(23)
         self.thetas = [self.theta.copy()]
         # Initialize the stepsize alpha
-        self.alpha = 0.01
+        self.alpha = 0.02
         # Initialize Epsilon for epsilon greedy
-        self.epsilon = 0.999
+        self.epsilon = 0.99999
         # Initialize agent parameters for stepsize rule        
         self.n=1
         self.stepsizes = [self.alpha]
@@ -70,38 +71,49 @@ class approximate_sarsa_agent_V3(object):
         # All negative states
         phi[2, :] = ((state[1:self.env.n_stores + 1] < 0) * 1).reshape(self.env.n_stores, 1)
         # Demand cannot be satisfied
-        phi[3, :] = (state[1:self.env.n_stores + 1] + action[:, 1:self.env.n_stores + 1] - 2 * state[
-                                                                                     self.env.n_stores + 1:2 * self.env.n_stores + 1] + state[
-                                                                                                                              2 * self.env.n_stores + 1:] < 0).T
-        # How much will there be after demand(only positive, 0 if can't be satisfied)
-        # Stock will be more than X after producing
-        # phi[2 + self.env.n_stores * 2, :] = (state[0] + action[:, 0] - sum(action[:, 1:].T) > 7)
-        # phi[2 + self.env.n_stores * 2 + 1, :] = (state[0] + action[:, 0] - sum(action[:,1:].T) > 15)
+        phi[3, :] = (state[1:self.env.n_stores + 1] + action[:, 1:self.env.n_stores + 1] - 2 * state[ self.env.n_stores + 1:2 * self.env.n_stores + 1] + state[ 2 * self.env.n_stores + 1:] < 0).T
         # Number of trucks being send
-        # phi[5, :] = (np.ceil(action[:, 1:self.env.n_stores + 1] / self.env.cap_truck)).T
+        phi[4, :] = (np.ceil(action[:, 1:self.env.n_stores + 1] / self.env.cap_truck)).T
+        # How empty is the truck
+        phi[5, :] = np.ceil(action[:, 1] / self.env.cap_truck) - action[:, 1] / self.env.cap_truck
+        # Penalty cost
+        phi[6,:] = np.minimum(np.zeros(s_shape), s_next[1:,:]) / 10
+        # Factory stock can satisfy next estimated demand
+        phi[7,:] = (s_next[0] >= np.sum(d_next))*1
+        # production rest
+        production_rest = (state[0] + action[:, 0] - sum(action[:, 1:].T))
+        #phi[8,:] = production_rest
+        # Stock will be in the % after producing and sending
+       # for period in range(1):
+            #if np.floor(((self.env.t%24))/3) == period:
+             #   production_rest = (state[0] + action[:, 0] - sum(action[:, 1:].T))
+            #else:
+            #    production_rest = -1
+        for i in range(10):
+            phi[8+i+0, :] = np.logical_and(production_rest > (store_cap[0]*0.1*i), production_rest <= (store_cap[0]*0.1*(i+1)))
+        #for period in range(8):
+        #    if np.floor(((self.env.t%24))/4) == period:
+        #        phi[8 + period * 2, :] = action[:, 0]
+        #        phi[9 + period * 2, :] = state[0] + action[:, 0] - sum(action[:, 1:].T)
+        #    else:
+        #        phi[8 + period * 2, :] = 0
+        #        phi[9 + period * 2, :] = 0
+        # Stock will be in the % after sending and possible demand
+        stock_rest = state[0] + action[:, 0] - sum(action[:, 1:].T)
+        for i in range(5):
+            phi[18+i, :] = np.logical_and(stock_rest > (store_cap[1] * 0.2 * i),  stock_rest <= (store_cap[1] * 0.2 * (i + 1)))
+        # Format output in case of single action input
+        if action_dim == 1:
+            return phi.reshape((theta_size,))
+        return phi
+
         # Will there be more than one truck of goods in the store?
         # phi[9, :] = state[1] + (action[:, 1] - (state[2] + state[3]) / 2).T > self.env.cap_truck
         # Will there be less or equal than one truck of goods in the store but more than 0?
         # phi[10, :] = np.logical_and((state[1] + (action[:, 1] - (state[2] + state[3]) / 2).T) <= self.env.cap_truck, (state[1] + (action[:, 1] - (state[2] + state[3]) / 2).T) >= 0)
         # More than 2 trucks of goods
         # phi[11, :] = state[1] + (action[:, 1] - (state[2] + state[3]) / 2).T > 2*self.env.cap_truck
-        # How full is the truck
-        phi[12, :] = np.ceil(action[:, 1] / self.env.cap_truck) - action[:, 1] / self.env.cap_truck
-        # Penalty cost
-        phi[13,:] = np.minimum(np.zeros(s_shape), s_next[1:,:]) / 100
-        # Factory stock can satisfy next estimated demand
-        phi[14,:] = (s_next[0] >= np.sum(d_next))*1
-        # Format output in case of single action input
-        production_rest = (state[0] + action[:, 0] - sum(action[:, 1:].T))
-        for i in range(10):
-            phi[15+i, :] = np.logical_and(production_rest > (store_cap[0]*0.1*i), production_rest <= (store_cap[0]*0.1*(i+1)))
-        stock_rest = state[0] + action[:, 0] - sum(action[:, 1:].T)
-        for i in range(4):
-            phi[25+i, :] = np.logical_and(stock_rest > (store_cap[1] * 0.1 * i),  stock_rest <= (store_cap[1] * 0.1 * (i + 1)))
-        if action_dim == 1:
-            return phi.reshape((theta_size,))
-        return phi
-            
+
     def get_action(self, state):
         # Find all possible actions
         actions = np.array(self.env.action_space())
@@ -124,10 +136,34 @@ class approximate_sarsa_agent_V3(object):
         self.thetas.append(self.theta.copy())
         
         # Update alpha, epsilon and n
-        self.epsilon *= 0.9999
+        self.epsilon *= 0.99999
         # self.alpha = update_alpha(self.alpha, self.n)
         #self.stepsizes.append(self.alpha)
         self.n+=1
         
         # Save information for log
         self.log.append([self.n, self.alpha, self.epsilon, delta, self.theta.copy()])
+
+    def create_plots(self, rewards):
+        '''
+            Plots parameters of agent
+        '''
+        # Receive information from agent
+        ns = [self.log[i][0] for i in range(len(self.log))]
+        alphas = [self.log[i][1] for i in range(len(self.log))]
+        epsilons = [self.log[i][2] for i in range(len(self.log))]
+        deltas = [self.log[i][3] for i in range(len(self.log))]
+        thetas = [self.log[i][4] for i in range(len(self.log))]
+
+        # Plot parameters
+        fig = plt.figure(figsize=(5, 10), dpi=120)
+        fig.add_subplot(6, 1, 1)
+        plt.plot(rewards)
+        fig.add_subplot(6, 1, 2)
+        plt.plot(thetas)
+        fig.add_subplot(6, 1, 3)
+        plt.plot(deltas)
+        fig.add_subplot(6, 1, 4)
+        plt.plot(alphas)
+        fig.add_subplot(6, 1, 5)
+        plt.plot(epsilons)
