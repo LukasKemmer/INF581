6050b500a7870ee46be593204d545027175622d6
Lukas Kemmer <lukaskemmer@Lukass-MacBook-Pro-2.local>
2018-03-07 09:55:27 +0100

    changes name of q s agent

--- code/evaluate_agent.py
+++ code/evaluate_agent.py
@@ -9,7 +9,7 @@ Created on Thu Feb 22 21:15:42 2018
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from q_s_agent import q_s_agent
+from s_q_agent import s_q_agent
 from approximate_sarsa import approximate_sarsa_agent
 #from approximate_sarsa_V2 import approximate_sarsa_agent_V2
 from approximate_sarsa_V3 import approximate_sarsa_agent_V3
